a40d8faa67bac66cd6cc3f3a1eea9d30cc8b8d1b
김민수 <gimminsu@gimminsuui-MacBookPro.local>
2021-07-20 02:00:54 +0900

    뭐든 바꿨겠지

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  api: {
-    bodyParser: false,
-  },
 };
--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -1,35 +1,35 @@
-import React, { useCallback, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { userActionTypes } from '../interface/iUserActType';
-import { RootState } from '../reducer/index';
-import wrapper from '../store/configure';
-import CommonHeader from '../components/CommonHeader';
-import styles from '../styles/layout.module.scss';
-import Section1 from '../components/LandingSection1';
-import Section2 from '../components/LandingSection2';
-import Section3 from '../components/LandingSection3';
-import Link from 'next/link';
-import Image from 'next/image';
-import CommonFooter from '../components/CommonFooter';
+import React, { useCallback, useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userActionTypes } from "../interface/iUserActType";
+import { RootState } from "../reducer/index";
+import wrapper from "../store/configure";
+import CommonHeader from "../components/CommonHeader";
+import styles from "../styles/layout.module.scss";
+import Section1 from "../components/LandingSection1";
+import Section2 from "../components/LandingSection2";
+import Section3 from "../components/LandingSection3";
+import Link from "next/link";
+import Image from "next/image";
+import CommonFooter from "../components/CommonFooter";
 
 const LandingPage = () => {
-	const { me } = useSelector((state: RootState) => state.user);
-	const dispatch = useDispatch();
+  const { me } = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch({
-			type: userActionTypes.LOAD_MYPROFILE_REQUEST,
-		});
-	}, []);
-	return (
-		<div className="container">
-			<CommonHeader />
-			<Section1 />
-			<Section2 />
-			<Section3 />
-			<CommonFooter />
-		</div>
-	);
+  // useEffect(() => {
+  // 	dispatch({
+  // 		type: userActionTypes.LOAD_MYPROFILE_REQUEST,
+  // 	});
+  // }, []);
+  return (
+    <div className="container">
+      <CommonHeader />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <CommonFooter />
+    </div>
+  );
 };
 
 export default LandingPage;
